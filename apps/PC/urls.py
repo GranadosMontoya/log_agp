4ea902304e337843de7a75a27f05fbc1a83da6b0
GranadosMontoya <sebasmontoya2107@gmail.com>
2025-09-02 20:26:31 -0500

    agregar y eliminar

--- apps/PC/urls.py
+++ apps/PC/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import PCView,DetallePCView
+from .views import PCView,DetallePCView, AgregarHistorialView, EliminarEquipoView
 app_name = 'pc'
 urlpatterns = [
     path('pc/', PCView.as_view(), name='pc_list'),
     path('pc/<str:serial>/', DetallePCView.as_view(), name='detalle_pc'),
+    path('detalle/<str:serial>/agregar_historial/', AgregarHistorialView.as_view(), name='agregar_historial'),
+    path('detalle/<str:serial>/eliminar/', EliminarEquipoView.as_view(), name='eliminar_equipo'),
+
 ]
--- apps/PC/views.py
+++ apps/PC/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import get_object_or_404, render
-from django.views.generic import ListView,View
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import ListView,View, DeleteView
 from apps.PC.models import Equipo,HistorialEquipo  # Ajusta si tu modelo está en otra app
+from .forms import HistorialEquipoForm
 
 class PCView(LoginRequiredMixin, ListView):
     paginate_by = 20  # Número de equipos por página
@@ -21,4 +23,40 @@ class DetallePCView(LoginRequiredMixin, View):
         equipo = Equipo.objects.get(serial=serial)
         print(equipo)
         historial = HistorialEquipo.objects.filter(equipo=equipo).order_by('-fecha')
-        return render(request, self.template_name, {'equipo': equipo, 'historial': historial})
+        form = HistorialEquipoForm()  # 👈 Aquí instanciamos el form vacío
+        return render(request, self.template_name, {'equipo': equipo, 'historial': historial,'form': form})
+    
+
+class AgregarHistorialView(LoginRequiredMixin, View):
+    login_url = 'user:login'
+
+    def post(self, request, serial):
+        equipo = get_object_or_404(Equipo, serial=serial)
+        form = HistorialEquipoForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            historial = form.save(commit=False)
+            historial.equipo = equipo
+            historial.save()
+            return redirect('pc:detalle_pc', serial=equipo.serial)
+
+        # Si no es válido, lo regresamos al detalle con errores
+        historial = HistorialEquipo.objects.filter(equipo=equipo).order_by('-fecha')
+        return render(
+            request,
+            'detalle_pc.html',
+            {
+                'equipo': equipo,
+                'historial': historial,
+                'form': form
+            }
+        )
+
+
+class EliminarEquipoView(LoginRequiredMixin, View):
+    login_url = 'user:login'
+
+    def post(self, request, serial):
+        equipo = Equipo.objects.get(serial=serial)
+        equipo.delete()
+        return redirect(reverse_lazy('pc:pc_list'))  # 🔹 Ajusta al nombre de tu vista/lista
